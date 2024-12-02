2d1deaa22d5526aa41e39ca780344e275c49bac4
MD IRSHAD <irshadidrisi9211@gmail.com>
2024-12-02 09:45:19 +0530

    Added Optimized Bubble Sort and Selection sort

--- sorting.cpp
+++ sorting.cpp
@@ -36,6 +36,7 @@ class Sorting{
         // 2. Swap the elements if they are in the wrong order
         // 3. Repeat the above steps for all the elements
         // 4. Repeat the above steps for n-1 iterations
+        // It is Stable Sorting
 
         //Give an in detail example of Bubble Sort Algorithm
         // 5 4 3 2 1 => 4 5 3 2 1 => 4 3 5 2 1 => 4 3 2 5 1 => 4 3 2 1 5
@@ -59,7 +60,51 @@ class Sorting{
                 }
             }
         }
-        
+
+        //Optimized Bubble Sort
+        void optimizedBubbleSort(){
+            for(int i=0; i<size; i++){
+                bool flag = false;
+                for(int j = 0; j<size - i - 1; j++){
+                    if(arr[j]>arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                        flag = true;
+                    }
+                }
+
+                if(!flag){
+                    break; 
+                }
+            }
+        }
+
+        //Selection sort
+        //Select the minimum element and put it at its correct ith position
+        //Select Correct Element and put it at its right ith position
+        //Best, Worst and Average Case Time Complexity: O(n^2)
+        //Not a Stable Sorting
+        //Used for small datasets
+
+
+        void selectionSort(){
+            for(int i = 0; i<size-1; i++){
+                int mini = i;
+                for(int j = i+1; j<size; j++){
+                    if(arr[mini] > arr[j]){
+                        mini = j;
+                    }
+                    swap(arr[i], arr[mini]);
+                }
+            }
+        }
+
+
+        //Insertion Sort
+        //
+
+         
 
         
 
@@ -74,7 +119,7 @@ int main(){
     Sorting s(5);
     s.input();
     s.traversal();
-    s.bubbleSort();
+    s.optimizedBubbleSort();
     s.traversal();
     return 0;
 }
